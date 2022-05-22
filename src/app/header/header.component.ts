ee2888a6157478bbf1c067222c28718a3d0ea670
dpi627 <dpi.studio@gmail.com>
2022-05-22 20:15:04 +0800

    interpolation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  sitename: string = 'Hello World';
 
-  constructor() { }
+  constructor() {}
 
+  //this is Hook method (like Event but not equal)
+  //Event can be subscribed, Hook can't
   ngOnInit(): void {
+    setTimeout(() => {
+      this.sitename = 'The Will Will Web';
+    }, 2000);
   }
-
 }
