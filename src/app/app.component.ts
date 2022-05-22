e11a35254cb579793a91420e42f9b533544a38d2
dpi627 <dpi.studio@gmail.com>
2022-05-22 23:32:40 +0800

    two-way binding demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng220522';
+  keyword = 'Andular';
+
+  changeKeyword(): void {
+    this.keyword = 'Hello World';
+  }
 }
