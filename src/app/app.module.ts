8b7680db702961ecf9c8a1bfa179feeb2c051901
dpi627 <dpi.studio@gmail.com>
2022-05-22 16:37:43 +0800

    remove page1 component manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Page1Component
+    AppComponent
   ],
   imports: [
     BrowserModule
