d9efe26de868f5f264ab057c8126f869a849bd89
dpi627 <dpi.studio@gmail.com>
2022-05-24 22:29:33 +0800

    implements piple method

--- src/app/mask.pipe.ts
+++ src/app/mask.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'mask'
+  name: 'mask',
 })
 export class MaskPipe implements PipeTransform {
+  transform(value: string, maskSymbol: string, showChars: number = 3): string {
+    let mask: string = '';
+    for (let i = 0; i < value.length - showChars * 2; i++) {
+      mask += maskSymbol;
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    return (
+      value.substring(0, showChars) +
+      mask +
+      value.substring(value.length - showChars, value.length)
+    );
   }
-
 }
